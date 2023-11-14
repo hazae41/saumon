f89ac4205ef39442b0ea2bd7bf7d42618570b73e
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-11-14 18:52:31 +0100

    space

--- src/mods/macros/run.ts
+++ src/mods/macros/run.ts
@@ -1,5 +1,11 @@
-export function $run$<T>(callback: () => T): Awaited<T> {
+export interface RunOptions {
+  readonly space?: number
+}
+
+export function $run$<T>(callback: () => T, options: RunOptions): Awaited<T> {
+  const { space = 2 } = options
+
   return (async () => {
-    return JSON.stringify(await callback())
+    return JSON.stringify(await callback(), undefined, space)
   })() as any
 }
