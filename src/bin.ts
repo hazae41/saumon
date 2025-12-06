7334b5155fcc9ffefac08e1851e5a605c58cdc45
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-12-06 18:37:36 +0100

    d

--- src/bin.ts
+++ src/bin.ts
@@ -14,8 +14,10 @@ process.addListener("exit", () => stack.dispose())
 process.addListener("SIGINT", () => process.exit(1))
 process.addListener("SIGTERM", () => process.exit(1))
 
-addEventListener?.("unhandledrejection", () => stack.dispose())
-addEventListener?.("error", () => stack.dispose())
+if ("addEventListener" in globalThis) {
+  addEventListener("unhandledrejection", () => stack.dispose())
+  addEventListener("error", () => stack.dispose())
+}
 
 const module = new URL("./mods/runner/mod.ts", import.meta.url)
 
